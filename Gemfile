8d1339b375f100c299fe1ccc3ce217ea508a7353
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2013-05-04 22:24:56 -0400

    cloned from base.

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source :rubygems
+
+gem 'compass'
+gem 'susy'
+
+gem 'coffee-script'
+gem 'uglifier'
---
+++ config.rb
@@ -0,0 +1,11 @@
+require 'susy'
+
+environment = :development
+
+http_path = "/"
+css_dir = "css"
+sass_dir = "css/src"
+images_dir = "img"
+javascripts_dir = "jsc"
+
+output_style = :expanded
